14f529e826855765ce574e70e34e8c1122b82b8b
Hari Mohanraj <hari.mohanraj89@gmail.com>
2021-04-17 11:55:18 +0530

    Do not break if setup and game initialization happen in any order

--- public/app.js
+++ public/app.js
@@ -7,16 +7,32 @@ socket.on('connect', () => {
 });
 
 socket.on('startGame', (data) => {
-    socket.emit('setGameState', {state: myGame.state()})
-    myGame.display();
-    myGame.board.displayGameBoard(window.innerWidth/3 , window.innerHeight/2);
-})
+    startGame(data);
+});
 
 socket.on('loadGame', (data) => {
-    myGame.setState(data.state);
-    myGame.display();
-    myGame.board.displayGameBoard(window.innerWidth/3 , window.innerHeight/2);
-})
+    loadGame(data);
+});
+
+function startGame(data) {
+    if (myGame) {
+        socket.emit('setGameState', {state: myGame.state()});
+        myGame.display();
+        myGame.board.displayGameBoard(window.innerWidth/3 , window.innerHeight/2);
+    } else {
+        setTimeout((() => startGame(data)), 1000);
+    }
+}
+
+function loadGame(data) {
+    if (myGame) {
+        myGame.setState(data.state);
+        myGame.display();
+        myGame.board.displayGameBoard(window.innerWidth/3 , window.innerHeight/2);
+    } else {
+        setTimeout((() => loadGame(data)), 1000);
+    }
+}
 
 function setup() {
     myGame = new Game(3);
@@ -24,17 +40,3 @@ function setup() {
     myGame.initPlayers();
     createCanvas(window.innerWidth, window.innerHeight);
 }
-
-function draw() {
-    
-}
-
-
-/** 
- 
-Client                   Server
-connect --------------->
-        <--------------- startGame
-setGameState ----------->
-        <--------------- setGameState
-**/
