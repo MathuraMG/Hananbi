025c373348e33ab41ab9c34c7aa389305e312a15
Hari Mohanraj <hari.mohanraj89@gmail.com>
2021-05-22 17:45:10 +0530

    Check next available game

--- index.js
+++ index.js
@@ -45,12 +45,26 @@ server.listen(port, () => {
 
 let io = require('socket.io').listen(server);
 
-function emptyLobby() {
-  return games["ABCD"].players.every((player) => !player);
+function emptyLobby(game) {
+  return game.players.every((player) => !player);
 }
 
-function nextAvailableSeat() {
-  return games["ABCD"].players.indexOf(null);
+function nextAvailableSeat(game) {
+  let seat = game.players.indexOf(null);
+
+  return (seat >= 0) ? seat : null;
+}
+
+function nextAvailableGame() {
+  for (const gameKey in games) {
+    let game = games[gameKey]
+    console.log("Checking game", gameKey)
+    if (nextAvailableSeat(game) !== null) {
+      return game;
+    }
+  }
+
+  return null;
 }
 
 function newPlayer(props) {
@@ -64,11 +78,14 @@ function newPlayer(props) {
 }
 
 io.sockets.on('connection', function(socket){
-  socket.join("ABCD");
-    if (emptyLobby()) {
-      let nextSeat = nextAvailableSeat();
+  let game = nextAvailableGame();
+
+  if (game) {
+    if (emptyLobby(game)) {
+      let nextSeat = nextAvailableSeat(game);
       console.log(`${socket.id}: Joined the game.`);
       console.log(`${socket.id}: Starting the game.`);
+      console.log(`${socket.id}: Sitting in seat ${nextSeat}.`);
 
       games["ABCD"].players[nextSeat] = newPlayer({playerNumber: nextSeat, id: socket.id});
 
@@ -79,36 +96,40 @@ io.sockets.on('connection', function(socket){
 
       io.to("ABCD").emit('loadProfiles', { profiles: games["ABCD"].players });
     } else {
-      let nextSeat = nextAvailableSeat();
+      let nextSeat = nextAvailableSeat(game);
       console.log(`${socket.id}: Joined the game.`);
 
-      if (nextSeat >= 0) {
+      if (nextSeat !== null) {
         console.log(`${socket.id}: Sitting in seat ${nextSeat}.`);
 
         games["ABCD"].players[nextSeat] = newPlayer({playerNumber: nextSeat, id: socket.id});
 
         io.to(socket.id).emit('loadGame', {
             state: games["ABCD"].gameState,
-            player: nextSeat
+            player: nextSeatx
         });
 
+        console.log()
         io.to("ABCD").emit('loadProfiles', { profiles: games["ABCD"].players });
       } else {
         console.log(`${socket.id}: Game full.`);
       }
     }
+  } else {
+    console.log(`${socket.id}: No games available.`);
+  }
 
-    socket.on('setGameState', function(data) {
-        games["ABCD"].gameState = data.state;
-        socket.to("ABCD").broadcast.emit('loadGame', { state: games["ABCD"].gameState });
-    })
+  socket.on('setGameState', function(data) {
+      games["ABCD"].gameState = data.state;
+      socket.to("ABCD").broadcast.emit('loadGame', { state: games["ABCD"].gameState });
+  })
 
-    //Listen for this client to disconnect
-    socket.on('disconnect', function() {
-        console.log(`${socket.id}: Left the game.`);
-        let playerNumber = games["ABCD"].players.findIndex((player) => (player && (socket.id === player.id)));
-        games["ABCD"].players[playerNumber] = null;
+  //Listen for this client to disconnect
+  socket.on('disconnect', function() {
+      console.log(`${socket.id}: Left the game.`);
+      let playerNumber = games["ABCD"].players.findIndex((player) => (player && (socket.id === player.id)));
+      games["ABCD"].players[playerNumber] = null;
 
-        io.to("ABCD").emit('loadProfiles', { profiles: games["ABCD"].players });
-    });
+      io.to("ABCD").emit('loadProfiles', { profiles: games["ABCD"].players });
+  });
 })
